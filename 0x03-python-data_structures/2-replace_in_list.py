4a2c6237a75fd4ee7abb045dc66b62dda23ea5d1
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-03 07:19:29 -0800

    Python

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in my_list:
+        if idx < 0:
+            my_list = my_list
+            return (my_list)
+        elif idx > len(my_list):
+            my_list = my_list
+            return (my_list)
+        elif idx == my_list.index(i):
+            my_list[idx] = element
+            return (my_list)
