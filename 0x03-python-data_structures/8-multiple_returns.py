9b5c3b876e2008119baa64c556d52b6889b2434d
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-05 07:48:17 -0800

    Python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    count = 0
     if (sentence == ''):
         sentence[0] == None
-        for i in sentence:
-            count += 1
-        b = ()
-        b = count, sentence[0]
+        b = len(sentence), sentence[0]
         return (b)
     else:
-        for i in sentence:
-            count += 1
-        b = ()
-        b = count, sentence[0]
+        b = len(sentence), sentence[0]
         return (b)
