06d88bf9be21fbb0ce85eea00a52885d1b59d9b1
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-05 03:51:29 -0800

    Python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     x = tuple_a
     y = tuple_b
 
-    if len(y) == 0:
-        return (x)
-    elif len(y) == 1:
-        b = ()
-        b = (x[0] + y[0], x[1] + 0)
-        return (b)
+    if len(y) < 2:
+        if len(y) == 1:
+            b = ()
+            b = (x[0] + y[0], x[1] + 0)
+            return (b)
+        elif len(y) == 0:
+            b = ()
+            b = (x[0] + 0, x[1] + 0)
+            return (b)
     elif len(x) > 2 or len(y) > 2:
         b = ()
         b = (x[0] + y[0], x[1] + y[1])
