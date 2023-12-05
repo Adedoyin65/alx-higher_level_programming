cc53a2ab5810c328ff7623ff3a19363448dccccf
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-05 07:27:29 -0800

    Python

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if (sentence == ""):
+        sentence[0] == None
+    else:
+        count = 0
+        for i in sentence:
+            count += 1
+        b = ()
+        b = count, sentence[0]
+        return (b)
