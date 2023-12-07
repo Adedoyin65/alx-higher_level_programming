e9fcbe0209f817d2848bb0f9786e17a9262450b7
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-07 12:13:33 -0800

    Python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    nl = my_list.copy()
     for i in my_list:
         if i == search:
-            idx = my_list.index(i)
-            my_list[idx] = replace
-            new = my_list
-            return (new)
+            nl[nl.index(i)] = replace
         else:
             continue
+    return (nl)
