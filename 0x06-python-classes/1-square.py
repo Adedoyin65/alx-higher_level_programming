55da2c7bd76bf3d3bb4429ddae5ebad03bdd813d
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-29 14:42:29 -0800

    Class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,13 +5,4 @@
 class Square:
     """A Square class having an init method."""
     def __init__(self, size):
-        """
-        __init__ is the initialization method
-
-        Args:
-            size(int): The size of the square
-        Note:
-            No type or value verification is performed
-        """
         self.__size = size
-
