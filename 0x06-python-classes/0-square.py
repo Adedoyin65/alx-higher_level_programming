67ccc0492dc721b00a66d550697d55cec94963f3
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-29 06:54:52 -0800

    Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is a module with empty class representing a square"""
+
+
 class Square:
     """Empty class representing a square."""
     pass
-    """The pass statement create a simple class with no function."""
