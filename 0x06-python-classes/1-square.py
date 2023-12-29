7af33e75fd7402504522edfd44bcb413bf3b0409
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2023-12-29 14:19:05 -0800

    Class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This is a module with a class Square."""
+
+
 class Square:
+    """A Square class having an init method."""
     def __init__(self, size):
         self.__size = size
 
