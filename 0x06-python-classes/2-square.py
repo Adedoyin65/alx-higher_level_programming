fb8ef829c892cf0757be41bf9d601513402287c3
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-03 03:19:15 -0800

    Class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class Square"""
+
+
 class Square:
     """A class Square that defines a square"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         A method called __init__ with args; self and size
         """
-        if not type(size) == int:
+        if not type(size) is not int:
             raise TypeError("size must be an integer")
         elif not size >= 0:
             raise ValueError("size must be >= 0")
