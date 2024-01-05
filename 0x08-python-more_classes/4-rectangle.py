c06f3bfbabd7b8d3fd5697ef15a3f64e1f7d36e5
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-05 10:11:01 -0800

    Class

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,5 +53,6 @@ class Rectangle:
             else:
                 dan += "#" * self.__width + "\n"
         return dan.rstrip()
+
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
