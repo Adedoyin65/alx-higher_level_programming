35f880c1b5ec3fd4ba5957249125371a42de1ee2
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-09 09:59:11 -0800

    Files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """A module that defines a function that reads a file"""
+
+
 def write_file(filename="", text=""):
     """A function that writes a file and returns the number of character"""
-    with open(filename, 'w', encoding='utf-8') as dan:
+    with open(filename, mode='w', encoding='utf-8') as dan:
         line = dan.write(text)
         return line
