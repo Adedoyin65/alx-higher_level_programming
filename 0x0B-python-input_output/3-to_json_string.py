210942a1644d9a3c3f9651ef971ef2897e777de0
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-09 12:47:17 -0800

    Files

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """A module that defines a function that returns json format of an object"""
 import json
 
+
 def to_json_string(my_obj):
     """A function that returns json representation of an object"""
     return json.dumps(my_obj)
