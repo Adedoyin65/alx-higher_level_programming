9b7813da69fff41ca59635609c6dbcc5c72d7542
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-08 20:38:23 -0800

    Inherit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""A module that defines a function to check if an object is an instance"""
+def inherits_from(obj, a_class):
+    """A function that returns True or False"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
