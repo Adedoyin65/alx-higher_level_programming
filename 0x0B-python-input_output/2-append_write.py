3cfb110e6e1fd105781e02534ee635966612eaf2
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-09 12:03:32 -0800

    Files

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A module that defines a function that appends"""
+
+
 def append_write(filename="", text=""):
     """A function that appends a text"""
     with open(filename, mode='a', encoding='utf-8') as dan:
