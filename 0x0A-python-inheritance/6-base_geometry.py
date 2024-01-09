810acb56a2d98635ae2ed02cc5c99474aceb183b
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-08 20:47:34 -0800

    Inherit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A module that defines a class BaseGeometry"""
+class BaseGeometry:
+    """A class called BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
