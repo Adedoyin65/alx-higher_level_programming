4485291e3c4d59736bbbc4fea23df24202cd1c9e
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-08 21:00:23 -0800

    Inherit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 """A module to check if an object is an instance"""
+
+
 def is_kind_of_class(obj, a_class):
+    """A function that returns True or False"""
     return isinstance(obj, a_class)
