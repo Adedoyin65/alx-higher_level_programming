07b45d7b9d67a6c5f789a10129201f802cc7561a
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-09 09:05:46 -0800

    Files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A module that defines a function that operate on files"""
+
+
 def read_file(filename=""):
     """A function that read a file and print it in stdout"""
     with open(filename, 'r', encoding='utf-8') as dan:
