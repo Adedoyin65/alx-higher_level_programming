b2f1987d40ebc68d7dcecb6c06dd406514f39420
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-10 00:24:07 -0800

    Files

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module that defines a function that returns a python object from a json format"""
+import json
+
+
+def from_json_string(my_str):
+    """A function that returns a python object from a json format"""
+    dan = json.loads(my_str)
+    return dan
