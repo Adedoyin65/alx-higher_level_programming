769398bd73d2e12fcde3576a16d2e0c3c0a7acc4
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-01-16 14:21:57 -0800

    Circles

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -66,8 +66,10 @@ class Rectangle(Base):
 
     def display(self):
         """This method displays a diagram representation of the instance"""
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x, "#" * self.__width)
 
     def __str__(self):
         """The str method"""
