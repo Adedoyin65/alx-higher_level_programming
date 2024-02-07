1fb69226ca4eaa1f10f0f753df799aad29aaaf9f
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-02-07 01:31:39 -0800

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,5 +19,28 @@ class TestRec(unittest.TestCase):
         self.assertEqual(foo.width, 7)
         foo.width = 9
         self.assertEqual(foo.width, 9)
+
+    def test_height(self):
+        boo = Rectangle(3, 5)
+        self.assertEqual(boo.height, 5)
+        boo.height = 10
+        self.assertEqual(boo.height, 10)
+
+    def test_x(self):
+        too = Rectangle(1, 1, 2)
+        self.assertEqual(too.x, 2)
+        too.x = 1
+        self.assertEqual(too.x, 1)
+
+    def test_y(self):
+        zoo = Rectangle(1, 1, 2, 6)
+        self.assertEqual(zoo.y, 6)
+        zoo.y = 7
+        self.assertEqual(zoo.y, 7)
+
+    def test_area(self):
+        roo = Rectangle(3, 4, 5, 6)
+        self.assertEqual(roo.area(), 12)
+
 if __name__ == '__main__':
     unittest.main()
