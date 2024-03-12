bf3c59dc424a4868ca57eafb8d5374c48e1c9318
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-12 16:51:58 -0700

    A class Rectangle base on 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-	  if (w === 0 || h === 0)
-	  {
-		  return {};
-	  }
-	  else if (w < 0 || h < 0)
-	  {
-		  return {};
+	  if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+		  return 'Rectangle{}';
 	  }
 	  this.width = w;
 	  this.height = h;
