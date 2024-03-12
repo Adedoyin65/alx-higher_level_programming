e58a95205186691f88dcf85ffff84519967a290c
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-11 18:16:25 -0700

    To print argv in Javascript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const [arg1] = process.argv.slice(2);
+const size = parseInt(arg1);
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
