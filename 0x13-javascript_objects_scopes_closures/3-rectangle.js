31ed2a819732d9427ef189fc54a39d40e12878ba
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-12 17:40:23 -0700

    A class Rectangle base on 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,8 @@
+#!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // Create an empty object
-      return {};
+      return {}; // Empty object
     }
     this.width = w;
     this.height = h;
@@ -17,7 +17,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
-        row += 'X';
+        row += 'X ';
       }
       console.log(row);
     }
