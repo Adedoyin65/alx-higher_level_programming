d141acb2e7ef95d971a97d338ee34ce9d6550e94
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-12 17:23:10 -0700

    A class Rectangle base on 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (!this.width || !this.height) {
+      console.log("Empty rectangle");
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X ';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
