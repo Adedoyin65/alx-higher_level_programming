03557fce81a292a7b9cf479a7fee8a7d7abc51b0
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-16 21:12:33 -0700

    A script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb
-dan = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="Eden",
-        passwd="ogunyemi65",
-        database="hbtn_0e_0_usa"
-        )
-mydan = dan.cursor()
-mydan.execute("SELECT * FROM states ORDER BY id ASC")
-res = mydan.fetchall()
-for i in res:
-    print(i)
+if __name__ == "__main__":
+    import MySQLdb
+    dan = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user="Eden",
+            passwd="ogunyemi65",
+            database="hbtn_0e_0_usa"
+            )
+    mydan = dan.cursor()
+    mydan.execute("SELECT * FROM states ORDER BY id ASC")
+    res = mydan.fetchall()
+    for i in res:
+        print(i)
