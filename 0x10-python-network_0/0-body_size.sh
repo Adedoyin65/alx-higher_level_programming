18eea1b2a95317aac00c9b1e63b7c85ef1beb969
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-28 13:42:07 -0700

    A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-URL=$1
-
-# Send a request to the URL and save the response body to a temporary file
-RESPONSE_FILE=$(mktemp)
-curl -s -o "$RESPONSE_FILE" "$URL"
-
-# Get the size of the response body in bytes
-SIZE=$(wc -c < "$RESPONSE_FILE")
-
-# Display the size of the response body
-echo "$SIZE"
-
-# Clean up the temporary file
-rm "$RESPONSE_FILE"
+URL=$1;file_1=$(mktemp);curl -s -o "$file_1" "$URL";echo "$(wc -c < "$file_1")"
