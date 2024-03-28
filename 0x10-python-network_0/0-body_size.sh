5beb20498c20b1104e37e14c59f14082741be413
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-03-28 13:04:57 -0700

    A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Get the URL from the command line argument
+# A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 URL=$1
 
 # Send a request to the URL and save the response body to a temporary file
