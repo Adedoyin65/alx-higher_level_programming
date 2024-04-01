741b3930936d6c7d72672d228faea8d10a83aeb6
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-01 15:34:32 -0700

    A python  script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
+import urllib.request
+import sys
+
+# Get the URL from command line arguments
+url = sys.argv[1]
+
+# Send request and read the response headers
+with urllib.request.urlopen(url) as response:
+    # Get the value of the X-Request-Id header if it exists
+    x_request_id = response.getheader('X-Request-Id')
+    if x_request_id:
+        print(f'{x_request_id}')
+    else:
+        print('X-Request-Id header not found in the response.')
