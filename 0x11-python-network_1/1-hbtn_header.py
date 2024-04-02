36592355a89714937cf805f7ed0c1958ead234ee
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-01 22:40:39 -0700

    A python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """A python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
-import urllib.request
+import urllib
 import sys
 
 # Get the URL from command line arguments
 url = sys.argv[1]
 
 # Send request and read the response headers
-with urllib.request.urlopen(url) as response:
-    # Get the value of the X-Request-Id header if it exists
-    x_request_id = response.getheader('X-Request-Id')
-    if x_request_id:
-        print(f'{x_request_id}')
-    else:
-        print('X-Request-Id header not found in the response.')
+response = urllib.urlopen(url)
+headers = response.info()
+
+# Get the value of the X-Request-Id header if it exists
+x_request_id = headers.get('X-Request-Id')
+if x_request_id:
+    print(f'{x_request_id}')
+else:
+    print('X-Request-Id header not found in the response.')
