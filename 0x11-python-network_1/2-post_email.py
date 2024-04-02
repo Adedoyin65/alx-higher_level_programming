e035e25f5705f8d974eb6ecedf0ea22061972f06
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-01 23:12:50 -0700

    A python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,20 +2,21 @@
 """A Python script that takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)"""
-import urllib.parse
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
 
-# Get the URL and email from command line arguments
-url = sys.argv[1]
-email = sys.argv[2]
+    # Get the URL and email from command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-# Encode the email data
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')  # Data should be bytes
+    # Encode the email data
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')  # Data should be bytes
 
-# Send POST request and read the response
-with urllib.request.urlopen(url, data=data) as response:
-    # Decode and print the response body (decoded in utf-8)
-    body = response.read().decode('utf-8')
-    print(body)
+    # Send POST request and read the response
+    with urllib.request.urlopen(url, data=data) as response:
+        # Decode and print the response body (decoded in utf-8)
+        body = response.read().decode('utf-8')
+        print(body)
