726a79aa40440cae8f9cb87c0fe9baf802611c8b
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-06 13:26:06 -0700

    A python script that prints in stdout the square with the character #

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -74,9 +74,7 @@ class Square:
         Raises:
             TypeError: If the value is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple) or len(value) != 2
-        or not all(isinstance(num, int) for num in value)
-        or any(num < 0 for num in value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) for num in value) or any(num < 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
