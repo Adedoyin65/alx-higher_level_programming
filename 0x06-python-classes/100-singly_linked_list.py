7a2509f73b112da7a3902026ac31f9eb1eaace66
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-06 14:10:50 -0700

    A class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -106,8 +106,8 @@ class SinglyLinkedList:
             self.head = new_node
         else:
             current = self.head
-            a = current.next_node.data
-            while current.next_node is not None and a < value:
+            a = current.next_node
+            while a is not None and current.next_node.data < value:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
