4b37bb98c0cd4a20f386615e0344703a046c4b9a
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-04-23 21:48:58 -0700

    A script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,39 +1,28 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Extract API URL from command line arguments
-const apiUrl = process.argv[2];
-
-// Make a GET request to the API URL
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  // Parse the JSON response body
-  const todos = JSON.parse(body);
-
-  // Initialize an object to store completed tasks by user ID
-  const completedTasksByUser = {};
-
-  // Iterate over each todo
-  todos.forEach(todo => {
-    // Check if the task is completed
-    if (todo.completed) {
-      // Increment the completed tasks count for the user
-      if (completedTasksByUser[todo.userId]) {
-        completedTasksByUser[todo.userId]++;
-      } else {
-        completedTasksByUser[todo.userId] = 1;
-      }
+function countCompletedTasksByUrl (apiUrl) {
+  request({ url: apiUrl, json: true }, (error, response) => {
+    if (error) {
+      console.error('Error fetching data:', error);
+      return;
     }
+    const todos = response.body;
+    const completedTasks = todos.filter(todo => todo.completed);
+    const completedCountByUser = completedTasks.reduce((acc, todo) => {
+      if (acc[todo.userId]) {
+        acc[todo.userId]++;
+      } else {
+        acc[todo.userId] = 1;
+      }
+      return acc;
+    }, {});
+    console.log(JSON.stringify(completedCountByUser, null, 2));
   });
-
-  // Print users with completed tasks and the number of completed tasks
-  console.log('Users with completed tasks:');
-  for (const userId in completedTasksByUser) {
-    console.log(`'${userId}': ${completedTasksByUser[userId]}`);
-  }
-});
+}
+const apiUrl = process.argv[2];
+if (!apiUrl) {
+  console.error('Usage: node script.js <API_URL>');
+  process.exit(1);
+}
+countCompletedTasksByUrl(apiUrl);
