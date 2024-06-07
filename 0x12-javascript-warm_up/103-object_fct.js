829bc38a82bbb44141697c0e1298e6dafca7b397
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-06-07 03:44:47 -0700

    Updated script

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value += 1;
-  }
+  value: 12
 };
 
 console.log(myObject);
+
+// Add the incr method to myObject
+myObject.incr = function incr () {
+  this.value += 1;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
