4e526548e5d7812f5751f4fb755a4b0197f83e76
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-06-08 09:04:26 -0700

    An sql script

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,17 +1,11 @@
 -- create_user.sql
 -- Script to create MySQL user user_0d_1 with all privileges
 
--- Check if user_0d_1 exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = user_0d_1 AND host = localhost) INTO @user_exists;
-
 -- Create user if not exists
-SET @create_user_query = IF(@user_exists, SELECT User already exists, CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd');
-PREPARE stmt FROM @create_user_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
