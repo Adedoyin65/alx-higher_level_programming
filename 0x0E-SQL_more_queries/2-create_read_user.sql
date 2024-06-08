0d52ac7c8d8eeed433c9a5a3f98ffb95ed801ee8
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-06-08 09:06:27 -0700

    An sql script

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- create_database_user.sql
+-- Script to create MySQL database hbtn_0d_2 and user user_0d_2
+
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+
+-- Apply the changes
+FLUSH PRIVILEGES;
