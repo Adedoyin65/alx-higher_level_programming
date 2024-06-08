d4c7a474a35b12e57bb877165830ae15fcc1f801
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-06-08 09:11:47 -0700

    An sql script

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,10 +5,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user if not exists
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Apply the changes
 FLUSH PRIVILEGES;
